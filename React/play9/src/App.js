4e4d05039ada97a8c2245742e2b5ab44a1d26c8e
mitekk <mitekk@gmail.com>
2018-02-17 23:55:22 +0200

    basic configuration

--- React/play9/src/App.js
+++ React/play9/src/App.js
@@ -1,20 +1,77 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
+import './styles.css';
 import './App.css';
+import _ from 'lodash/lodash.js';
+
+const Stars = (props) => {
+  const numberOfStars = Math.floor(Math.random()*9) + 1;
+  let stars=[];
+  for(let i=0; i< numberOfStars; i++){
+   stars.push(<i key={i} className="fa fa-star"></i>);
+  }
+
+  return (
+    <div className="col-5">
+      {stars}
+    </div>
+  );
+}
+
+const Button = (props) => {
+  return (
+    <div className="col-2">
+      <button>=</button>
+    </div>
+  );
+}
+
+const Answer = (props) => {
+  return (
+    <div className="col-5">
+      ...
+    </div>
+  );
+}
+
+const Numbers = (props) => {
+  const arrayOfNumbers = _.range(1,10);
+  return (
+    <div className="card text-center">
+      <div>
+        {arrayOfNumbers.map((number,index)=>{
+          <span key={index}>{number}</span>
+        })}
+      </div>
+    </div>
+  );
+}
+
+class Game extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h3>Play Nine</h3>
+        <hr />
+        <div className="row">
+          <Stars />
+          <Button />
+          <Answer />
+        </div>
+        <br />
+        <Numbers />
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return (<div>
+      <Game />
+    </div>);
   }
 }
 
