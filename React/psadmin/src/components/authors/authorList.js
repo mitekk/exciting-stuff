e5888cb52cb6af8f37716c0f865dcb991c378c3d
mitekk <mitekk@gmail.com>
2018-02-25 23:35:18 +0200

    component composition + life cycle

---
+++ React/psadmin/src/components/authors/authorList.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var React = require('react');
+var PropTypes = require('prop-types');
+
+var AuthorList = React.createClass({
+    propTypes: {
+        authors: PropTypes.array.isRequired
+    },
+    render: function () {
+        var createAuthorRow = function (author) {
+            return (
+                <tr key={author.id}>
+                    <td><a href={"/#authors/" + author.id}>{author.id}</a></td>
+                    <td>{author.firstName} {author.lastName}</td>
+                </tr>
+            );
+        };
+        return (
+            <div>
+                <table className="table">
+                    <thead>
+                        <th>ID</th>
+                        <th>Name</th>
+                    </thead>
+                    <tbody>
+                        {this.props.authors.map(createAuthorRow, this)}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+});
+
+module.exports = AuthorList;
